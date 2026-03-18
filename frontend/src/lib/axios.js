d0d4cb3d46e8ce3eabb180dea65407e14bf33e55
Harsh Bhikadiya <harsh.bhikadiya55@gmail.com>
2026-03-18 10:09:01 +0530

    fix baseUrl of axios in frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_API,
+    baseURL: import.meta.env.VITE_BASE_API + "/api",
     withCredentials: true,
 }); 
